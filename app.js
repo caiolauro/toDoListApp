c20f0291e58183aaed5e1fde37d43f51877a3e31
Caio <Caio>
2023-05-04 16:23:21 -0300

    EJS Layouts

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ app.post('/work', (req, res) => {
     res.redirect("/work");
 })
 
+app.get("/about", function (req, res) {
+    res.render('about');
+});
 app.listen(3000, function () {
     console.log("Server started at port 3000.");
 });
